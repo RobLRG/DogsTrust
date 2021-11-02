7bbc6a7ad8d34fd289dc7eaa0ab9a367edad3809
RobLRG <bobpeters5889@hotmail.com>
2021-11-02 15:14:32 +0000

    added form to add extra dogs, added delete button

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 let dogs=[] //creates the dogs array
 
+function saveDog(e){
+
+    let name2=document.getElementById("name").value
+    let age2=document.getElementById("age").value
+    let breed2=document.getElementById("breed").value
+
+    dogs.push(new Dogs(name2,breed2,age2,""))
+
+     showDogs()
+     
+     e.preventDefault();
+     return false
+
+}
+
+
+
 function showDogs() //functions used to create the cards
 {
+    let dogsPanel=document.getElementById("cardHolder")
+    dogsPanel.innerHTML=""
+    console.log(dogs)
+
     for(let i=0;i<dogs.length;i++)   //takes the length of the dogs array which changes depending on how many dogs i have created and then runs the code inside it that many times
     {
         //creates card ------
@@ -39,18 +60,28 @@ function showDogs() //functions used to create the cards
         {
             alert("sorry this dog has been adopted already")
         }
+
+        let deleteButton=document.createElement("button")
+        deleteButton.innerHTML = "delete"
+        card.appendChild(deleteButton)
+        deleteButton.classList.add("adoptbutton")
+        deleteButton.onclick = function clicked()
+        {
+            card.parentNode.removeChild(card)
+        }
     }
 }
 
-
 function setupDogs() //fucntion to setup the dog instances and info
 {
+    //dogs.push(new Dogs(this.name, this.breed,this.age))
     dogs.push(new Dogs("Winnie", "Staffy", "9" , "/IMG/winnie.jpg"))             
     dogs.push(new Dogs("Otto", "Dachshund/Terrier", "10" , "/IMG/otto.jpg"))       
     dogs.push(new Dogs("Otis", "Husky", "4" , "/IMG/husky.jpg"))                   
     dogs.push(new Dogs("Cheyenne", "German Shepherd", "6" , "/IMG/Cheyenne.jpg"))   
     dogs.push(new Dogs("Bianca", "Golden Retriever", "8" , "/IMG/golden.jpg"))          
-    dogs.push(new Dogs("Hana", "Shiba Inu", "3" , "/IMG/Shiba.jpg"))    
+    dogs.push(new Dogs("Hana", "Shiba Inu", "3" , "/IMG/Shiba.jpg"))
+    
 }
 
 class Dogs{ //class for all the dog instances
@@ -61,7 +92,7 @@ class Dogs{ //class for all the dog instances
     age 
     picture
 
-    constructor(name,breed,age,picture)
+    constructor(name,breed,age,picture,)
     {
         this.name=name
         this.breed=breed
@@ -72,5 +103,46 @@ class Dogs{ //class for all the dog instances
     
 }
 
-setupDogs() //runs the function creating the dog instances
-showDogs()  //runs the function creating the cards so the dogs will actually show
+function showMessage(input, message, type) {
+	const msg = input.parentNode.querySelector("small");
+	msg.innerText = message;
+	input.className = type ? "success" : "error";
+	return type;
+}
+
+function showError(input, message) {
+	return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+	return showMessage(input, "", true);
+}
+
+function hasValue(input, message) {
+	if (input.value.trim() === "") {
+		return showError(input, message);
+	}
+	return showSuccess(input);
+}
+
+const form = document.querySelector("#signup");
+
+const NAME_REQUIRED = "Please enter a name";
+const AGE_REQUIRED = "Please enter an age";
+const BREED_REQUIRED = "please enter a breed";
+
+form.addEventListener("submit", function (event) {
+	event.preventDefault();
+
+	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
+	let ageValid = hasValue(form.elements["age"], AGE_REQUIRED);
+    let breedValid = hasValue(form.elements["breed"], BREED_REQUIRED);
+
+	if (nameValid && ageValid && breedValid) {
+        alert("works")
+        saveDog()
+	}
+});
+
+setupDogs() 
+showDogs() 
